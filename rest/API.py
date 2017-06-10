4dee381973cf1a8f4b4f3c95298e1b6cd6cc271b
gaarangoa <gaarangoa@gmail.com>
2017-06-10 00:24:50 -0400

    fix

--- rest/API.py
+++ rest/API.py
@@ -23,7 +23,7 @@ def batchPredict():
     try:
         sentiment = {'positive':0, 'negative':0}
         data = request.json;
-        return jsonify(data=request)
+        return jsonify(data=data)
 
         # for i in data['documents']:
         #     prediction = model.predict(i)
